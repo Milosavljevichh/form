227a7084e009a9f56c4afc55c8cf38d92f205f0f
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-08-03 22:25:06 +0200

    added confirm password validation

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 
-let firstName = document.getElementById("firstName");
 
-firstName.onchange = () => {
-    console.log('changed');
-}
+let passwordField = document.getElementById("password");
+let confirmPasswordField = document.getElementById("confirm-password");
+let submitBtn = document.getElementById("create-profile");
+
+submitBtn.addEventListener('click', checkPassword);
+
+function checkPassword(e) {
+    
+    let valid = true;
+    if (passwordField.value !== confirmPasswordField.value) {
+        confirmPasswordField.style.border = "solid 3px red"
+        confirmPasswordField.willValidate = false;
+        confirmPasswordField.validationMessage = "Passwords do not match";
+        console.log('majke ti ga izbokarim')
+        e.preventDefault();
+    }
+    return valid;
+};
+
+confirmPasswordField.addEventListener('change', function() {
+    if (passwordField.value !== confirmPasswordField.value) {
+        confirmPasswordField.style.border = "solid 3px red"
+    } else {
+        confirmPasswordField.style.border = "1px solid gray";
+    }
+});
+
+passwordField.addEventListener('change', function() {
+    if (passwordField.value !== confirmPasswordField.value) {
+        confirmPasswordField.style.border = "solid 3px red"
+    } else {
+        confirmPasswordField.style.border = "1px solid gray";
+    }
+});
